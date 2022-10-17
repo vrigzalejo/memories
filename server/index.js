d195bff04ba43808b68d5ffb369e94faa4268815
Brigido Alejo <brigsalejo@gmail.com>
2022-10-18 02:39:44 +0800

    Added posts route

--- server/index.js
+++ server/index.js
@@ -4,9 +4,13 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import * as dotenv from 'dotenv'
 
+import postRoutes from './routes/posts.js'
+
 dotenv.config()
 
 const app = express()
+
+app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
