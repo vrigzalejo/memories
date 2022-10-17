72a71fddf9b5c8a188f0af0474d28510b889794a
Brigido Alejo <brigsalejo@gmail.com>
2022-10-18 02:27:56 +0800

    Added dotenv package

--- server/index.js
+++ server/index.js
@@ -1,8 +1,26 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-// import mongoose from 'mongoose'
-// import cors from 'cors'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
-app.use(bodyParser.json)
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+
+const CONNECTION_URL = `mongodb+srv://${process.env.MONGO_ATLAS_USER}:
+${process.env.MONGO_ATLAS_PASS}@cluster0.gjlxidw.mongodb.net/?
+retryWrites=true&w=majority`
+
+const PORT = process.env.PORT || 5000
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.log(error.message))
