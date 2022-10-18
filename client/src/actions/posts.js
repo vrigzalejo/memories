1d0860768a7b9d72b66d34cd7d3d26ffcebac3e8
Brigido Alejo <brigsalejo@gmail.com>
2022-10-18 22:12:45 +0800

    Creating post and fetching it works

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -5,8 +5,18 @@ export const getPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts();
 
-        dispatch({ type: 'FETCH_ALL', payload: [] })
+        dispatch({ type: 'FETCH_ALL', payload: data })
     } catch (error) {
         console.log(error.message)
     }
+}
+
+export const createPost = (post) => async (dispatch) => {
+  try {
+    const { data } = await api.createPost(post)
+
+    dispatch({ type: 'CREATE', payload: data })
+  } catch (error) {
+    console.log(error.message)
+  }
 }
