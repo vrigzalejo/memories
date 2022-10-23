f84892ffcc636fa5727112f6c8b4111699c2ccef
Brigido Alejo <brigsalejo@gmail.com>
2022-10-23 17:21:24 +0800

    Delete post is working

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -14,8 +14,12 @@ import moment from 'moment'
 
 import useStyles from './styles'
 
+import { useDispatch } from 'react-redux'
+import { deletePost } from '../../../actions/posts'
+
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles()
+  const dispatch = useDispatch()
 
   return (
     <Card className={classes.card}>
@@ -56,7 +60,7 @@ const Post = ({ post, setCurrentId }) => {
           Like
           {post.likeCount}
         </Button>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
           <DeleteIcon fontSize="small" />
           Delete
         </Button>
