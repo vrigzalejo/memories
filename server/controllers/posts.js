9e6e1b5f850bb4b328c13768ddb0f2cdf473a155
Brigido Alejo <brigsalejo@gmail.com>
2022-10-23 15:46:17 +0800

    Fixed issue in updatePost route

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -29,7 +29,7 @@ export const updatePost = async (req, res) => {
   const { id: _id } = req.params
   const post = req.body
 
-  if (mongoose.Types.ObjectId.isValid(_id)) {
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
     return res.status(404).send('No post with that id')
   }
 
